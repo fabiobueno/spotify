85f61e7b432e7e88c51889236e14cedef874058c
Fabio Bueno <fabioo.bueno@gmail.com>
2019-11-18 15:48:48 -0300

    Started redux config

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+
+import { Provider } from "react-redux";
 import GlobalStyle from "./styles/global";
 import { BrowserRouter } from "react-router-dom";
 
@@ -10,21 +12,26 @@ import { Wrapper, Container, Content } from "./styles/components";
 
 import Routes from "./routes";
 
+import "./config/reactotron";
+import store from "./store";
+
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyle></GlobalStyle>
-      <Wrapper>
-        <Container>
-          <Sidebar></Sidebar>
-          <Content>
-            <Header></Header>
-            <Routes></Routes>
-          </Content>
-        </Container>
-        <Player></Player>
-      </Wrapper>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalStyle></GlobalStyle>
+        <Wrapper>
+          <Container>
+            <Sidebar></Sidebar>
+            <Content>
+              <Header></Header>
+              <Routes></Routes>
+            </Content>
+          </Container>
+          <Player></Player>
+        </Wrapper>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+import { createStore, compose, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+
+import sagas from "./sagas";
+import reducers from "./ducks";
+
+const middlewares = [];
+
+const sagaMonitor =
+  process.env.NODE_ENV === "development"
+    ? console.tron.createSagaMonitor()
+    : null;
+
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
+
+middlewares.push(sagaMiddleware);
+
+const store = createStore(reducers, compose(applyMiddleware(...middlewares)));
+
+sagaMiddleware.run(sagas);
+
+export default store;
